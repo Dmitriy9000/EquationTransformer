e626ec0ac03ab025c1d9bd68fa147b317f6762a9
dmitriy.kudinov <d.koudinov@gmail.com>
2019-03-06 17:12:53 +0300

    Summation class has been added

---
+++ EquationTransformer/EquationProcessor/Summand.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace EquationTransformer.EquationProcessor
+{
+    public class Summand
+    {
+        public float Multiplier { get; private set; }
+        public int Power { get; private set; }
+        public string Variable { get; private set; }
+
+        public Summand(string toParse)
+        {
+            try
+            {
+                // Power parsing
+                var remain = toParse;
+                if (toParse.Contains("^"))
+                {
+                    var powerParts = remain.Split('^');
+                    if (powerParts.Length != 2)
+                    {
+                        throw new Exception($"Incorrect summand format: {toParse}");
+                    }
+                    remain = powerParts[0];
+                    Power = int.Parse(powerParts[1]);
+                }
+                else
+                {
+                    Power = 1;
+                }
+
+                // Multiplier & Variable parsing
+                remain = remain.Replace("(", string.Empty);
+                remain = remain.Replace(")", string.Empty);
+
+                if (remain[0] == '-')
+                {
+                    Multiplier = -1;
+                    remain = remain.Remove(0, 1);
+                }
+                else
+                {
+                    Multiplier = 1;
+                }
+
+                var regex = new Regex(@"(?<mult>[+|-]?\d?[.]?\d)?(?<v>\w+)?");
+                var match = regex.Match(remain);
+
+                if (!match.Success)
+                    throw new Exception($"Incorrect summand format: {toParse}");
+
+                if (match.Groups["mult"].Success)
+                {
+                    var mult = match.Groups["mult"];
+                    if (!string.IsNullOrWhiteSpace(match.Groups["mult"].Value))
+                    {
+                        Multiplier *= float.Parse(match.Groups["mult"].Value);
+                    }
+                }
+                else
+                    Multiplier *= 1;
+
+                if (match.Groups["v"].Success)
+                {
+                    if (string.IsNullOrWhiteSpace(match.Groups["v"].Value))
+                    {
+                        Variable = string.Empty;
+                    }
+                    else
+                    {
+                        Variable = match.Groups["v"].Value;
+                    }
+                }
+                else
+                    Variable = string.Empty;
+            }
+            catch (Exception exception)
+            {
+                throw new Exception($"Incorrect summand format: {toParse}");
+            }
+        }
+    }
+}
---
+++ EquationTransformerTests/EquationProcessor/SummandTests.cs
@@ -0,0 +1,117 @@
+﻿using EquationTransformer.EquationProcessor;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EquationTransformerTests.EquationProcessor
+{
+    [TestClass]
+    public class SummandTests
+    {
+        [TestMethod]
+        public void SummandParse1()
+        {
+            var summand = new Summand("3.5(xy)^2");
+            Assert.AreEqual(3.5, summand.Multiplier);
+            Assert.AreEqual("xy", summand.Variable);
+            Assert.AreEqual(2, summand.Power);
+        }
+
+        [TestMethod]
+        public void SummandParse2()
+        {
+            var summand = new Summand("(xy)^2");
+            Assert.AreEqual(1, summand.Multiplier);
+            Assert.AreEqual("xy", summand.Variable);
+            Assert.AreEqual(2, summand.Power);
+        }
+
+        [TestMethod]
+        public void SummandParse3()
+        {
+            var summand = new Summand("0.5(xy)");
+            Assert.AreEqual(0.5, summand.Multiplier);
+            Assert.AreEqual("xy", summand.Variable);
+            Assert.AreEqual(1, summand.Power);
+        }
+
+        [TestMethod]
+        public void SummandParse4()
+        {
+            var summand = new Summand("3x^2");
+            Assert.AreEqual(3, summand.Multiplier);
+            Assert.AreEqual("x", summand.Variable);
+            Assert.AreEqual(2, summand.Power);
+        }
+
+        [TestMethod]
+        public void SummandParse5()
+        {
+            var summand = new Summand("3.5x");
+            Assert.AreEqual(3.5, summand.Multiplier);
+            Assert.AreEqual("x", summand.Variable);
+            Assert.AreEqual(1, summand.Power);
+        }
+
+        [TestMethod]
+        public void SummandParse6()
+        {
+            var summand = new Summand("x^2");
+            Assert.AreEqual(1, summand.Multiplier);
+            Assert.AreEqual("x", summand.Variable);
+            Assert.AreEqual(2, summand.Power);
+        }
+
+        [TestMethod]
+        public void SummandParse7()
+        {
+            var summand = new Summand("3.5xy");
+            Assert.AreEqual(3.5, summand.Multiplier);
+            Assert.AreEqual("xy", summand.Variable);
+            Assert.AreEqual(1, summand.Power);
+        }
+
+        [TestMethod]
+        public void SummandParse8()
+        {
+            var summand = new Summand("y");
+            Assert.AreEqual(1, summand.Multiplier);
+            Assert.AreEqual("y", summand.Variable);
+            Assert.AreEqual(1, summand.Power);
+        }
+
+        [TestMethod]
+        public void SummandParse9()
+        {
+            var summand = new Summand("-3.5xy");
+            Assert.AreEqual(-3.5, summand.Multiplier);
+            Assert.AreEqual("xy", summand.Variable);
+            Assert.AreEqual(1, summand.Power);
+        }
+
+        [TestMethod]
+        public void SummandParse10()
+        {
+            var summand = new Summand("+3.5xy");
+            Assert.AreEqual(3.5, summand.Multiplier);
+            Assert.AreEqual("xy", summand.Variable);
+            Assert.AreEqual(1, summand.Power);
+        }
+
+        [TestMethod]
+        public void SummandParse11()
+        {
+            var summand = new Summand("-2");
+            Assert.AreEqual(-2, summand.Multiplier);
+            Assert.AreEqual(string.Empty, summand.Variable);
+            Assert.AreEqual(1, summand.Power);
+        }
+
+        [TestMethod]
+        public void SummandParse12()
+        {
+            var summand = new Summand("-y");
+            Assert.AreEqual(-1, summand.Multiplier);
+            Assert.AreEqual("y", summand.Variable);
+            Assert.AreEqual(1, summand.Power);
+        }
+    }
+}
